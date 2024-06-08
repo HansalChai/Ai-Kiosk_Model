9c86064ddcde1867bf1b71f75f2da5ceb16774a1
cho1n <cjaewon0217@naver.com>
2024-06-08 22:13:34 +0900

    Feat: Cors 설정 추가 및 최종본 배포완료

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware  # CORS 미들웨어 추가
 import uvicorn
 from typing import List
 import subprocess
@@ -8,13 +9,26 @@ import os
 
 app = FastAPI()
 
-@app.post("/detect/")
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/predict/")
 async def create_upload_files(file: UploadFile = File(...)):
     with open("temp.jpg", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     
     # detect.py를 subprocess로 실행
-    result = subprocess.run(['python3', '/Users/choejaewon/Desktop/Capstone/test2/Gender-and-Age-Detection/detect.py', '--image', 'temp.jpg'], capture_output=True, text=True)
+    result = subprocess.run(['python3', './Gender-and-Age-Detection/detect.py', '--image', 'temp.jpg'], capture_output=True, text=True)
     
     # 결과 파싱 (연령 정보만 추출)
     age_info = result.stdout.strip()  # 연령 정보만 있는 줄을 기반으로, 앞뒤 공백을 제거합니다.
@@ -23,4 +37,4 @@ async def create_upload_files(file: UploadFile = File(...)):
     return JSONResponse(content={"message": "Detection completed", "age": age_info})
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
